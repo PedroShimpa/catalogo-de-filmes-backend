d67040cdd57b4f1d0f333f915491ee2b5a8fc4aa
Pedro Falconi <pedrofalck2000@gmail.com>
2024-05-22 15:23:03 -0300

    redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
 // app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { CatalogModule } from './modules/catalog/catalog.module';
-import { DatabaseOptions, RedisOptions } from './configs/app-options-constants';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(DatabaseOptions),
-    CacheModule.registerAsync(RedisOptions),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     AuthModule,
     UserModule,
     CatalogModule,
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -7,16 +7,30 @@ export class RedisService {
   private client;
 
   constructor() {
-    this.client = redis.createClient();
+    this.client = redis.createClient({
+      legacyMode: true,
+      url: 'redis://redis:6379'
+  });
   }
 
   async get(key: string): Promise<any> {
-    const getAsync = promisify(this.client.get).bind(this.client);
+    if (!this.client.connected) { 
+    await this.client.connect()
+    }
+    const getAsync = await promisify(this.client.get).bind(this.client);
     const value = await getAsync(key);
+
+    await  this.client.disconnect();
+
     return JSON.parse(value);
   }
 
   async set(key: string, value: any): Promise<void> {
-    this.client.set(key, JSON.stringify(value));
+    if (!this.client.connected) { 
+      await this.client.connect()
+    }
+
+    await this.client.set(key, JSON.stringify(value));
+    await  this.client.disconnect();
   }
 }
