3f30d03f6c2a3ca639bfcb53cd484c95e448ca6d
Pedro Falconi <pedrofalck2000@gmail.com>
2024-05-22 01:13:08 -0300

    correções

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,14 @@ RUN npm install
 
 COPY . .
 
+ENV DATABASE_HOST=postgres \
+	DATABASE_PORT=postgres \
+	DATABASE_USERNAME=postgres \
+	DATABASE_PASSWORD=postgres \
+	DATABASE_NAME=desafio \
+	REDIS_HOST=redis \
+	JWT_SECRET=macacosmemordam
+
 EXPOSE 3000
 
 RUN npm run build
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  //auto doc creator
+  const config = new DocumentBuilder()
+    .setTitle('Catalogo de Filme')
+    .setDescription('Desafio catalogo de filmes backend em nest js, postgress e redis')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
