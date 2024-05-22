55ca3878aeef75151e74f54517ee9023b9210b11
Pedro Falconi <pedrofalck2000@gmail.com>
2024-05-22 00:55:15 -0300

    Update catalogo.controller.ts

--- src/modules/catalogo/catalogo.controller.ts
+++ src/modules/catalogo/catalogo.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	UseGuards,
+	Request,
+	Delete
+  } from "@nestjs/common"
 import { CatalogoService } from './catalogo.service';
 import { Public } from '../auth/public.decorator';
 import { AuthGuard } from '../auth/auth.guard';
 import { ProfileDto } from '../auth/dto/profile.dto';
 import { CreateFilmeDto } from './dto/create-filme.dto';
 import { Catalogo } from './entities/catalogo.entity';
+import { UpdateFilmeDto } from "./dto/update-filme.dto";
 
 @Controller('catalogo')
 export class CatalogoController {
@@ -26,5 +37,25 @@ export class CatalogoController {
 	@Post()
 	create(@Body() createFilmeDto: CreateFilmeDto): Promise<Catalogo> {
 		return this.catalogoService.create(createFilmeDto)
+	}
+
+	@Get(":id")
+	findOne(@Param("id") id: string): Promise<Catalogo> {
+		return this.catalogoService.findOne(id)
+	}
+
+	@Patch(":id")
+	update(
+		@Param("id") id: string,
+		@Body() updateFilmeDto: UpdateFilmeDto,
+	) {
+		return this.catalogoService.update(id, updateFilmeDto)
+	}
+
+	@Delete(":id")
+	delete(
+		@Param("id") id: string,
+	) {
+		return this.catalogoService.remove(id)
 	}
 }
