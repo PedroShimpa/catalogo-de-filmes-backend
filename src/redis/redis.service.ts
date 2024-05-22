e8f19c8f13ea2034bd9b943b20d56cb511944948
Pedro Falconi <pedrofalck2000@gmail.com>
2024-05-22 15:28:41 -0300

    Update redis.service.ts

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -10,27 +10,19 @@ export class RedisService {
     this.client = redis.createClient({
       legacyMode: true,
       url: 'redis://redis:6379'
-  });
+    });
+    if (!this.client.connected) {
+      this.client.connect()
+    }
   }
 
   async get(key: string): Promise<any> {
-    if (!this.client.connected) { 
-    await this.client.connect()
-    }
     const getAsync = await promisify(this.client.get).bind(this.client);
     const value = await getAsync(key);
-
-    await  this.client.disconnect();
-
     return JSON.parse(value);
   }
 
   async set(key: string, value: any): Promise<void> {
-    if (!this.client.connected) { 
-      await this.client.connect()
-    }
-
     await this.client.set(key, JSON.stringify(value));
-    await  this.client.disconnect();
   }
 }
